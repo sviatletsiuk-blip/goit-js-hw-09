49e363297804e1b4ee9b957744c80edd5639db83
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-01-31 22:25:08 +0200

    initial commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,48 @@
-console.log("Form");
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+// 1. Обʼєкт поза будь-якими функціями
+let formData = {
+  email: "",
+  message: "",
+};
+
+// 2. Перевіряємо localStorage при завантаженні сторінки
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email ?? "";
+  formData.message = parsedData.message ?? "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+// 3. Делегування події input
+form.addEventListener("input", event => {
+  const { name, value } = event.target;
+
+  if (name in formData) {
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+// 4. Обробка submit
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  // Очищення
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+  form.reset();
+});
